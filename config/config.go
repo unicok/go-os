e6582b9267c17b4b578b630ff0c4458e55b65e15
Asim <asim@chuhnk.me>
2016-01-08 22:10:26 +0000

    First crack at the config interface

--- config/config.go
+++ config/config.go
@@ -1,1 +1,86 @@
 package config
+
+import (
+	"time"
+)
+
+// Config is the top level config which aggregates a
+// number of sources and provides a single merged
+// interface.
+type Config interface {
+	// Loads config from sources
+	Load() error
+	// Config values
+	Values
+	// Combined source
+	Source
+	// Options for the config
+	Options() Options
+	// Start/Stop for internal interval updater, etc.
+	Start() error
+	Stop() error
+}
+
+// Source is the source from which config is loaded.
+// This may be a file, a url, consul, etc.
+type Source interface {
+	// Loads ChangeSet from the source
+	Read() (ChangeSet, error)
+	// Watch for changes
+	Watch() Watcher
+}
+
+// Values loaded within the config
+type Values interface {
+	// The path could be a nested structure so
+	// make it a composable.
+	Get(path ...string) Value
+}
+
+// Represent a value retrieved from the values loaded
+type Value interface {
+	Bool() bool
+	Int(def int) int
+	String(def string) string
+	Float64(def float64) float64
+	Duration(def string) time.Duration
+	StringSlice() []string
+	StringMap() map[string]string
+	Struct(val interface{}) error
+	Bytes() []byte
+}
+
+// ChangeSet represents a set an actual source
+type ChangeSet interface {
+	// The time at which the last change occured
+	Timestamp() time.Time
+	// The raw data set for the change
+	Data() []byte
+	// Hash of the source data
+	Checksum() string
+	// The source of this change
+	String() string
+}
+
+// The watcher notifies of changes at a granular level.
+// Changes() can be called multiple times to retrieve
+// new channels. When Stop() is called, all channels
+// are closed and the watcher is rendered unusable.
+type Watcher interface {
+	// Retrieve a watcher on the source
+	Changes() chan ChangeSet
+	// Stop all channels
+	Stop() error
+}
+
+/*
+
+Scoped config by labels, environment, datacenter, etc
+type Context interface {}
+
+Validate loaded properties
+type Validator interface {}
+
+*/
+
+type Option func(o *Options)
---
+++ config/options.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"time"
+)
+
+type Options struct {
+	PollInterval time.Duration
+	Sources      []Source
+}
+
+// PollInterval is the time interval at which the sources are polled
+// to retrieve config.
+func PollInterval(i time.Duration) Option {
+	return func(o *Options) {
+		o.PollInterval = i
+	}
+}
+
+// WithSource appends a source to our list of sources.
+// This forms a hierarchy whereby all the configs are
+// merged down with the last specified as favoured.
+func WithSource(s Source) Option {
+	return func(o *Options) {
+		o.Sources = append(o.Sources, s)
+	}
+}
