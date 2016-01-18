b319a74daa821a9b80eba69932cb8d1c98146ab6
Asim <asim@chuhnk.me>
2016-01-18 18:24:31 +0000

    Router interface

--- router/router.go
+++ router/router.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"github.com/micro/go-micro/selector"
+)
+
+// The router is the client interface to a
+// global service loadbalancer (GSLB).
+// Metrics are batched and published to
+// a router which has a view of the whole
+// system.
+type Router interface {
+	selector.Selector
+	Start() error
+	Stop() error
+}
