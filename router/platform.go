9be822aa2e24943c055b240edf0445b1365f160d
Asim <asim@chuhnk.me>
2016-01-31 23:25:29 +0000

    First on router

--- router/platform.go
+++ router/platform.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -10,14 +12,13 @@ import (
 	"github.com/micro/go-micro/selector/blacklist"
 	"github.com/micro/go-micro/server"
 
+	"github.com/micro/go-platform/router/proto"
+
 	"golang.org/x/net/context"
 )
 
 type platform struct {
 	opts selector.Options
-	sync.RWMutex
-	running bool
-	exit    chan bool
 
 	// fallback selector
 	selector selector.Selector
@@ -28,6 +29,11 @@ type platform struct {
 	// TODO
 	// selector cache service:[]versions
 	// stats cache client calls service:[]stats
+
+	sync.RWMutex
+	running bool
+	exit    chan bool
+	stats   map[string]*stats
 }
 
 var (
@@ -59,16 +65,71 @@ func newPlatform(opts ...selector.Option) Router {
 		client:   c,
 		server:   s,
 		selector: blacklist.NewSelector(selector.Registry(options.Registry)),
+		stats:    make(map[string]*stats),
+	}
+}
+
+func serviceToProto(s *registry.Service) *router.Service {
+	if s == nil || len(s.Nodes) == 0 {
+		return nil
+	}
+	return &router.Service{
+		Name:     s.Name,
+		Version:  s.Version,
+		Metadata: s.Metadata,
+		Nodes: []*router.Node{&router.Node{
+			Id:       s.Nodes[0].Id,
+			Address:  s.Nodes[0].Address,
+			Port:     int64(s.Nodes[0].Port),
+			Metadata: s.Nodes[0].Metadata,
+		}},
 	}
 }
 
 func (p *platform) publish() {
-	// TODO: publish stats
-	return
+	p.RLock()
+	defer p.RUnlock()
+
+	opts := p.server.Options()
+
+	var addr, host string
+	var port int
+
+	if len(opts.Advertise) > 0 {
+		addr = opts.Advertise
+	} else {
+		addr = opts.Address
+	}
+
+	parts := strings.Split(addr, ":")
+	if len(parts) == 2 {
+		i, _ := strconv.ParseInt(parts[1], 10, 64)
+		host = parts[0]
+		port = int(i)
+	} else {
+		host = addr
+	}
+
+	service := &registry.Service{
+		Name:    opts.Name,
+		Version: opts.Version,
+		Nodes: []*registry.Node{&registry.Node{
+			Id:       opts.Id,
+			Address:  host,
+			Port:     port,
+			Metadata: opts.Metadata,
+		}},
+	}
+
+	// publish all the stats
+	for _, stat := range p.stats {
+		pub := p.client.NewPublication(StatsTopic, stat.ToProto(service))
+		go p.client.Publish(context.TODO(), pub)
+	}
 }
 
 func (p *platform) subscribe() {
-	// TODO: subscribe to stats
+	// TODO: subscribe to stream of updates from router
 	return
 }
 
@@ -114,6 +175,14 @@ func (p *platform) Options() selector.Options {
 	return p.opts
 }
 
+func (p *platform) Record(r Request, node *registry.Node, d time.Duration, err error) {
+	return
+}
+
+func (p *platform) Stats() ([]*Stats, error) {
+	return nil, nil
+}
+
 func (p *platform) Select(service string, opts ...selector.SelectOption) (selector.Next, error) {
 	// TODO: read from cache
 	// fallback to selector
@@ -121,15 +190,31 @@ func (p *platform) Select(service string, opts ...selector.SelectOption) (select
 }
 
 func (p *platform) Mark(service string, node *registry.Node, err error) {
-	// TODO: update stats
-	// also update selector
+	p.Lock()
+	defer p.Unlock()
+
 	p.selector.Mark(service, node, err)
+
+	stats, ok := p.stats[node.Id]
+	if !ok {
+		return
+	}
+	stats.Mark(service, node, err)
 }
 
 func (p *platform) Reset(service string) {
-	// reset stats?
-	// also update selector
+	p.Lock()
+	defer p.Unlock()
+
 	p.selector.Reset(service)
+
+	// reset counters and stats
+	for _, stat := range p.stats {
+		if stat.service.Name != service {
+			continue
+		}
+		stat.Reset()
+	}
 }
 
 func (p *platform) Close() error {
--- router/router.go
+++ router/router.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"time"
+
+	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-micro/selector"
 )
 
@@ -10,10 +13,36 @@ import (
 // a router which has a view of the whole
 // system.
 type Router interface {
+	// Provides the selector interface
 	selector.Selector
+	// Return stats maintained here
+	Stats() ([]*Stats, error)
+	// Record stats for a request - too many args ugh
+	Record(r Request, node *registry.Node, d time.Duration, err error)
+	// Start/Stop internal publishing, caching, etc
 	Start() error
 	Stop() error
 }
+
+type Stats struct {
+	Service   *registry.Service
+	Client    *registry.Service
+	Timestamp int64
+	Duration  int64
+	// TODO:
+	// Selected
+	// Endpoints
+}
+
+// Could be client or server request
+type Request interface {
+	Service() string
+	Method() string
+}
+
+var (
+	StatsTopic = "platform.router.stats"
+)
 
 func NewRouter(opts ...selector.Option) Router {
 	return newPlatform(opts...)
